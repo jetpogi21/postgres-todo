61dfe21eaf93f24f0dfe3b847303703248312ac4
jetpogi21 <jet_pradas@yahoo.com>
2023-10-31 11:05:22 +0800

    Footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <div className="h-[50px] flex items-center justify-between max-w-screen-lg w-full mx-auto px-4 text-xs select-none">
-      <div>©2023 Pradascus. All rights reserved.</div>
+      <div>&copy; 2023 Pradascus. All rights reserved.</div>
     </div>
   );
 };
